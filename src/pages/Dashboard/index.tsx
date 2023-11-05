114993e93cc776f85f31d1e317c74f630247c8e6
Augusto <augustolfp@gmail.com>
2023-11-05 20:38:44 -0300

    feat: implement base layout

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,28 +1,36 @@
 import { useAppContext } from "../../contexts/AppContext";
 
 export default function Dashboard() {
-    const { round, nextRound, submit, distance, map, userMarker, panorama } =
-        useAppContext();
+    const { round, nextRound, submit, distance } = useAppContext();
 
     return (
-        <div>
-            <div id="panorama" className="w-48 h-48"></div>
-            <div id="map" className="w-48 h-48"></div>
-            <button className="btn btn-primary" onClick={submit}>
-                Submit guess
-            </button>
-            <button className="btn btn-secondary" onClick={nextRound}>
-                Skip/Next
-            </button>
-            {distance && (
-                <div>
-                    <span className="font-semibold">Distance: </span>
-                    {distance}
+        <div className="w-screen h-screen">
+            <div className="relative h-5/6">
+                <div id="panorama" className="w-full h-full" />
+
+                <div className="absolute bottom-0 right-0 z-10 w-10 h-10 md:w-1/4 md:hover:w-2/5 md:h-1/3 md:hover:h-1/2 md:bottom-6 md:right-6 transition-all">
+                    <div className="w-full h-full">
+                        <div id="map" className="h-full w-full"></div>
+                    </div>
                 </div>
-            )}
-            <div>
-                <span className="font-semibold">Round:</span>
-                {round}
+            </div>
+            <div className="px-6 flex items-center gap-2 h-1/6">
+                <button className="btn btn-primary" onClick={submit}>
+                    Submit guess
+                </button>
+                <button className="btn btn-secondary" onClick={nextRound}>
+                    Skip/Next
+                </button>
+                {distance && (
+                    <div>
+                        <span className="font-semibold">Distance: </span>
+                        {distance}
+                    </div>
+                )}
+                <div>
+                    <span className="font-semibold">Round:</span>
+                    {round}
+                </div>
             </div>
         </div>
     );

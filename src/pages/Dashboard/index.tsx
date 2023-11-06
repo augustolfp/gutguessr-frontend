5da44fbfc2394f4eae23a6d62557af7ac2043a55
Augusto <augustolfp@gmail.com>
2023-11-05 21:46:17 -0300

    refactor: move collapsible tray into separate component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,44 +1,14 @@
 import { useAppContext } from "../../contexts/AppContext";
 import InfoFooter from "../../components/InfoFooter";
-import { useState } from "react";
-import { FaMapMarked } from "react-icons/fa";
-import { AiOutlineClose } from "react-icons/ai";
+import CollapsibleTray from "../../components/CollapsibleTray";
 
 export default function Dashboard() {
-    const { submit } = useAppContext();
-    const [isTrayOpen, setIsTrayOpen] = useState(false);
-
-    let trayStyle: string = isTrayOpen
-        ? "absolute bottom-0 right-0 z-10 w-full h-2/3 md:bottom-4 md:right-4 md:w-1/2 md:h-1/2 transition-all"
-        : "absolute bottom-0 right-0 z-10 w-14 h-14 md:bottom-4 md:right-4 transition-all";
-
-    let mapContainerStyle: string = isTrayOpen
-        ? "h-full w-full opacity-100"
-        : "h-full w-full opacity-0";
     return (
         <div className="w-screen h-screen">
             <div className="relative h-5/6">
                 <div id="panorama" className="w-full h-full" />
 
-                <div className={`${trayStyle}`}>
-                    <div className="flex flex-col gap-1 h-full w-full">
-                        <div id="map" className={`${mapContainerStyle}`}></div>
-                        {isTrayOpen && (
-                            <button
-                                className="btn btn-primary w-full"
-                                onClick={submit}
-                            >
-                                Submit guess!
-                            </button>
-                        )}
-                        <button
-                            className="btn btn-neutral btn-sm md:btn-md aspect-square rounded-full p-2 absolute top-1 right-1 z-20"
-                            onClick={() => setIsTrayOpen((prev) => !prev)}
-                        >
-                            {isTrayOpen ? <AiOutlineClose /> : <FaMapMarked />}
-                        </button>
-                    </div>
-                </div>
+                <CollapsibleTray />
             </div>
             <InfoFooter />
         </div>

4b4c7152d4b79dbb56df7784f8ee3c3b99900248
Augusto <augustolfp@gmail.com>
2023-11-12 15:58:56 -0300

    feat: implement Homepage base layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { AppProvider } from "./contexts/AppContext";
 
+import Navbar from "./components/Navbar";
 import Homepage from "./pages/Homepage";
 import Dashboard from "./pages/Dashboard";
 
@@ -9,6 +10,7 @@ function App() {
     return (
         <Router>
             <AppProvider>
+                <Navbar />
                 <Routes>
                     <Route path="/" element={<Homepage />} />
                     <Route path="/single-player" element={<Dashboard />} />
--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function Homepage() {
     return (
-        <div className="container mx-auto">
-            <h1>Homepage</h1>
-            <Link className="btn btn-primary" to="/single-player">
-                Single Player mode
-            </Link>
+        <div className="container mx-auto h-screen mt-12 p-6 flex flex-col justify-center items-center">
+            <div className="flex flex-wrap justify-center items-center gap-4">
+                <Link className="btn btn-primary" to="/single-player">
+                    Singleplayer mode
+                </Link>
+                <button className="btn btn-primary">Multiplayer mode</button>
+            </div>
         </div>
     );
 }

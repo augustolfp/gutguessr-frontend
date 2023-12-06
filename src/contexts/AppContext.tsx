851a36ca9f7c41991afb130a4066da693b3460c4
Augusto Lopes <augustolfp@gmail.com>
2023-12-06 16:04:42 -0300

    refactor: remove unnecessary exports from AppContext

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -1,7 +1,7 @@
 import calculateScore from "../utils/calculateScore";
 import { axiosClient } from "../config/axios";
 import { Loader } from "@googlemaps/js-api-loader";
-import { createContext, useState, useEffect, useContext } from "react";
+import { createContext, useState, useContext } from "react";
 import {
     initMap,
     initStreetView,
@@ -36,9 +36,6 @@ interface MapContext {
     round: number;
     nextRound: () => void;
     submit: () => Promise<void>;
-    map: google.maps.Map | null;
-    userMarker: google.maps.marker.AdvancedMarkerElement | null;
-    panorama: google.maps.StreetViewPanorama | null;
     scores: ScoreObj[];
     init: () => void;
     seeds: Seed[];
@@ -55,7 +52,6 @@ export function AppProvider({ children }: ProviderProps) {
     const [scores, setScores] = useState<ScoreObj[]>([]);
     const [seeds, setSeeds] = useState<Seed[]>([]);
     const [round, setRound] = useState(0);
-    // const { lat, lng, heading, pitch } = seeds[round];
     const [map, setMap] = useState<google.maps.Map | null>(null);
     const [userMarker, setUserMarker] =
         useState<google.maps.marker.AdvancedMarkerElement | null>(null);
@@ -112,9 +108,6 @@ export function AppProvider({ children }: ProviderProps) {
             value={{
                 round,
                 nextRound,
-                map,
-                userMarker,
-                panorama,
                 submit,
                 scores,
                 init,

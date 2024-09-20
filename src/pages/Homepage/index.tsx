199ff5558856b2a14efa2530755bdab9d484961b
augustolfp <augustolfp@gmail.com>
2024-09-20 19:52:44 -0300

    feat: add instructions on homepage

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -4,6 +4,7 @@ import CreateSinglePlayerSession from "../../components/CreateSinglePlayerSessio
 export default function Homepage() {
     return (
         <div className="container mx-auto h-screen mt-12 p-6 flex flex-col justify-center items-center">
+            <p className="py-8 font-semibold text-primary text-center">Só um aviso: a primeira rodada pode demorar alguns segundinhos para carregar! Abraços :)</p>
             <div className="flex flex-wrap justify-center items-center gap-4">
                 <CreateSinglePlayerSession />
             </div>

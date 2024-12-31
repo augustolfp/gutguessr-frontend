c14b4933f18a9cb52bc7f94dbd101cbe627ffbb1
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2024-12-31 19:51:07 -0300

    feat: implement Rio de Janeiro background

--- src/pages/Homepage/Background.tsx
+++ src/pages/Homepage/Background.tsx
@@ -1,14 +1,14 @@
-import Image2 from "./silhouettes/silhueta2.svg";
+import RioDeJaneiro from "./silhouettes/rio.svg";
 
 export default function Background() {
   return (
     <div className="">
       <img
-        id="image2"
-        src={Image2}
+        id="rio-de-janeiro"
+        src={RioDeJaneiro}
         className="absolute bottom-1/2 left-0 w-full"
       />
-      <div className="absolute bottom-0 left-0 w-full h-[50vh] bg-[#4a044e]"></div>
+      <div className="absolute bottom-0 left-0 w-full h-[50vh] bg-[#FFD200] opacity-25"></div>
     </div>
   );
 }

e2144fb44b6577af637c93f010dc427c400aa89b
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2024-12-31 20:14:33 -0300

    feat: adjust background image size for different viewports

--- src/pages/Homepage/Background.tsx
+++ src/pages/Homepage/Background.tsx
@@ -1,12 +1,14 @@
 import RioDeJaneiro from "../../assets/rioDeJaneiro.svg";
 
+import "./background.css";
+
 export default function Background() {
   return (
     <div>
       <img
         id="rio-de-janeiro"
         src={RioDeJaneiro}
-        className="absolute bottom-1/4 left-0 w-full"
+        className="absolute bottom-1/4 left-0 h-[400px]"
       />
       <div className="absolute bottom-0 left-0 w-full h-[25vh] bg-[#FFD200] opacity-25"></div>
     </div>

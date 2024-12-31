ab088ebfde57bbb8a146a30308ac2f1160754cb5
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2024-12-31 01:22:50 -0300

    feat: add play now button on homepage

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,13 +1,33 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import CreateSinglePlayerSession from "../../components/CreateSinglePlayerSession";
 
 export default function Homepage() {
-    return (
-        <div className="container mx-auto h-screen mt-12 p-6 flex flex-col justify-center items-center">
-            <p className="py-8 font-semibold text-primary text-center">Só um aviso: a primeira rodada pode demorar alguns segundinhos para carregar! Abraços :)</p>
-            <div className="flex flex-wrap justify-center items-center gap-4">
-                <CreateSinglePlayerSession />
-            </div>
-        </div>
-    );
+  const [menu, setMenu] = useState("DEFAULT");
+
+  return (
+    <div className="container mx-auto h-screen mt-12 p-6 flex flex-col justify-center items-center">
+      <p className="py-8 font-semibold text-primary text-center">
+        Só um aviso: a primeira rodada pode demorar alguns segundinhos para
+        carregar! Abraços :)
+      </p>
+      <div className="flex flex-col items-center">
+        <p>VOCÊ MANJA DE GEOGRAFIA?</p>
+        <p>
+          No GutGuessr você será transportado para qualquer lugar do mundo,
+          desde Paris na França, até Barro Duro no Piauí.
+        </p>
+        {menu === "DEFAULT" && (
+          <button
+            onClick={(_e) => {
+              setMenu("SINGLE_PLAYER_SESSION_FORM");
+            }}
+            className="btn btn-primary"
+          >
+            JOGAR AGORA!
+          </button>
+        )}
+        {menu === "SINGLE_PLAYER_SESSION_FORM" && <CreateSinglePlayerSession />}
+      </div>
+    </div>
+  );
 }

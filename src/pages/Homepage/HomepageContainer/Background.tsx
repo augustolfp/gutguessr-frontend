3728c78fa2f133b4f0c5a9625c057c1cceb929be
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-01-02 00:35:09 -0300

    feat: move rio background to the bottom of page

--- src/pages/Homepage/HomepageContainer/Background.tsx
+++ src/pages/Homepage/HomepageContainer/Background.tsx
@@ -8,9 +8,8 @@ export default function Background() {
       <img
         id="rio-de-janeiro"
         src={RioDeJaneiro}
-        className="absolute bottom-1/4 left-0"
+        className="absolute bottom-0 left-0"
       />
-      <div className="absolute bottom-0 left-0 w-full h-[25vh] bg-[#FFD200] opacity-25"></div>
     </div>
   );
 }

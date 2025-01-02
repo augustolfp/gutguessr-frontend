656e1d8eb6527407da15277c2b57947114703326
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-01-02 04:05:21 -0300

    Revert "feat: transform rio de janeiro svg into react component"
    This reverts commit 0281434aa3d8306508b162a89ba9ca6e48a01fdf.

--- src/pages/Homepage/HomepageContainer/Background.tsx
+++ src/pages/Homepage/HomepageContainer/Background.tsx
@@ -1,13 +1,13 @@
-import Rio from "./Rio";
+import RioDeJaneiro from "../../../assets/rioDeJaneiro.svg";
 
 import "./background.css";
 
 export default function Background() {
   return (
-    <div className="absolute bottom-0 left-0">
-      <div className="rio-container">
-        <Rio />
-      </div>
-    </div>
+    <img
+      id="rio-de-janeiro"
+      src={RioDeJaneiro}
+      className="absolute bottom-0 left-0"
+    />
   );
 }

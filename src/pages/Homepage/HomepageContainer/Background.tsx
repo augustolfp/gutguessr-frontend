1992e6c9c3a2d8e80552e56bd0ab640a9450c38c
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-01-02 00:37:56 -0300

    feat: remove navbar from homepage

--- src/pages/Homepage/HomepageContainer/Background.tsx
+++ src/pages/Homepage/HomepageContainer/Background.tsx
@@ -4,12 +4,10 @@ import "./background.css";
 
 export default function Background() {
   return (
-    <div>
-      <img
-        id="rio-de-janeiro"
-        src={RioDeJaneiro}
-        className="absolute bottom-0 left-0"
-      />
-    </div>
+    <img
+      id="rio-de-janeiro"
+      src={RioDeJaneiro}
+      className="absolute bottom-0 left-0"
+    />
   );
 }

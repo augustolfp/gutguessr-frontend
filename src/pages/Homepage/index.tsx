0d88c21f1125c73ca6ad5abe3f9f93bc2736889a
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-01-09 00:57:10 -0300

    refactor: move homepage hero to separate component

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import CreateSinglePlayerSession from "../../components/CreateSinglePlayerSession";
 import HowToPlay from "../../components/HowToPlay";
 import HomepageContainer from "./HomepageContainer";
+import HomepageHero from "./HomepageHero";
 
 export default function Homepage() {
   const [menu, setMenu] = useState("DEFAULT");
@@ -9,29 +10,7 @@ export default function Homepage() {
   return (
     <HomepageContainer>
       <div className="container mx-auto pt-16 flex flex-col justify-center items-center relative">
-        <h1 className="text-2xl font-extrabold md:text-4xl lg:text-5xl">
-          <span className="text-primary">Gut</span>Guessr
-        </h1>
-        <p className="text-4xl font-black mt-12 md:text-5xl lg:text-7xl">
-          Explore o Mundo
-        </p>
-        <span>
-          <div className="relative text-4xl font-black tracking-wider md:text-5xl lg:text-7xl">
-            <span className="bg-clip-text bg-gradient-to-r from-secondary via-primary to-accent blur-3xl opacity-50 absolute">
-              em um novo nível
-            </span>
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-secondary via-primary to-accent">
-              em um novo nível
-            </span>
-          </div>
-        </span>
-        <p className="text-center text-base font-light text-base-content text-opacity-70 mt-4 md:text-lg lg:text-2xl lg:leading-8">
-          No GutGuessr você será transportado para <br /> qualquer lugar do
-          planeta: <br /> desde Paris na França, até Barro Duro no Piauí.
-        </p>
-        <p className="text-base font-light text-base-content text-opacity-70 mt-4 md:text-lg lg:text-2xl">
-          Tudo isso de graça e sem sair de casa!
-        </p>
+        <HomepageHero />
         {menu === "DEFAULT" && (
           <>
             <button

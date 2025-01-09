bacf669a33af0dcc345aa5a93e91ddbe376b79a9
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-01-08 23:20:49 -0300

    feat: adjust font sizes on homepage

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -9,12 +9,14 @@ export default function Homepage() {
   return (
     <HomepageContainer>
       <div className="container mx-auto pt-16 flex flex-col justify-center items-center relative">
-        <h1 className="text-5xl font-extrabold">
+        <h1 className="text-2xl font-extrabold md:text-4xl lg:text-5xl">
           <span className="text-primary">Gut</span>Guessr
         </h1>
-        <p className="text-7xl font-black mt-12">Explore o Mundo</p>
+        <p className="text-4xl font-black mt-12 md:text-5xl lg:text-7xl">
+          Explore o Mundo
+        </p>
         <span>
-          <div className="relative text-7xl font-black tracking-wider">
+          <div className="relative text-4xl font-black tracking-wider md:text-5xl lg:text-7xl">
             <span className="bg-clip-text bg-gradient-to-r from-secondary via-primary to-accent blur-3xl opacity-50 absolute">
               em um novo nível
             </span>
@@ -23,11 +25,11 @@ export default function Homepage() {
             </span>
           </div>
         </span>
-        <p className="text-center text-2xl font-light text-base-content text-opacity-70 leading-8 mt-4">
+        <p className="text-center text-base font-light text-base-content text-opacity-70 mt-4 md:text-lg lg:text-2xl lg:leading-8">
           No GutGuessr você será transportado para <br /> qualquer lugar do
           planeta: <br /> desde Paris na França, até Barro Duro no Piauí.
         </p>
-        <p className="text-2xl font-light text-base-content text-opacity-70 mt-4">
+        <p className="text-base font-light text-base-content text-opacity-70 mt-4 md:text-lg lg:text-2xl">
           Tudo isso de graça e sem sair de casa!
         </p>
         {menu === "DEFAULT" && (
@@ -36,7 +38,7 @@ export default function Homepage() {
               onClick={(_e) => {
                 setMenu("SINGLE_PLAYER_SESSION_FORM");
               }}
-              className="btn btn-primary btn-lg mt-8"
+              className="btn btn-primary mt-8 lg:btn-lg"
             >
               Jogar Agora!
             </button>
@@ -44,7 +46,7 @@ export default function Homepage() {
               onClick={(_e) => {
                 setMenu("HOW_TO_PLAY");
               }}
-              className="btn btn-neutral btn-lg mt-6"
+              className="btn btn-neutral mt-2 lg:btn-lg"
             >
               Como Jogar?
             </button>

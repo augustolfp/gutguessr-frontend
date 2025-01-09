e5428d96ef4154686316bc69fadb9f895bd55c76
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-01-09 01:31:42 -0300

    feat: implement CreateSinglePlayerSession modal

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,48 +1,36 @@
-import { useState } from "react";
 import CreateSinglePlayerSession from "./CreateSinglePlayerSession";
-// import HowToPlay from "./HowToPlay";
 import HomepageContainer from "./HomepageContainer";
 import HomepageHero from "./HomepageHero";
 
 export default function Homepage() {
-  const [menu, setMenu] = useState("DEFAULT");
+  const handleOpenModal = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
 
+    const modal = document.getElementById(
+      "createSinglePlayerSessionModal"
+    ) as HTMLDialogElement;
+
+    modal.showModal();
+  };
   return (
     <HomepageContainer>
       <div className="container mx-auto pt-16 flex flex-col justify-center items-center relative">
         <HomepageHero />
-        {menu === "DEFAULT" && (
-          <>
-            <button
-              onClick={(_e) => {
-                setMenu("SINGLE_PLAYER_SESSION_FORM");
-              }}
-              className="btn btn-primary mt-8 lg:btn-lg"
-            >
-              Jogar Agora!
-            </button>
-            {/* <button
-              onClick={(_e) => {
-                setMenu("HOW_TO_PLAY");
-              }}
-              className="btn btn-neutral mt-2 lg:btn-lg"
-            >
-              Como Jogar?
-            </button> */}
-          </>
-        )}
-        {menu !== "DEFAULT" && (
-          <button
-            onClick={(_e) => {
-              setMenu("DEFAULT");
-            }}
-            className="btn btn-secondary"
-          >
-            Voltar
-          </button>
-        )}
-        {menu === "SINGLE_PLAYER_SESSION_FORM" && <CreateSinglePlayerSession />}
-        {/* {menu === "HOW_TO_PLAY" && <HowToPlay />} */}
+        <button
+          onClick={handleOpenModal}
+          className="btn btn-primary mt-8 lg:btn-lg"
+        >
+          Jogar Agora!
+        </button>
+        <dialog id="createSinglePlayerSessionModal" className="modal">
+          <div className="modal-box">
+            <CreateSinglePlayerSession />
+          </div>
+          <form method="dialog" className="modal-backdrop">
+            <button>close</button>
+          </form>
+        </dialog>
       </div>
     </HomepageContainer>
   );
